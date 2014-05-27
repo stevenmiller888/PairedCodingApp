ce2bbeda8503385eed0ad52599bddef0ac78d51e
stevenmiller888 <stevenmiller888@me.com>
2014-05-27 16:55:52 -0700

    added levels

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :profile_name, :email, :password)}
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :profile_name, :email, :password, :level)}
   end
 end
