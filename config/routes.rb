cce44dea22d771e39b84f26d710d45577812d40d
stevenmiller888 <stevenmiller888@me.com>
2014-05-28 12:15:18 -0700

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   #Routes for friendships
   resources :friendships
 
-<<<<<<< HEAD
   #Route for the ajax get request to the other editor's document
   get '/documents', to: 'documents#index'
   #Route for the ajax put request to update the current_user's editor's document's text
@@ -17,12 +16,13 @@ Rails.application.routes.draw do
   #Route for running the code
   post '/documents', to: 'documents#run'
 
-  #Route for the about us page 
-=======
+  #Route for the ajax get request to the other editor's document
+
   get '/documents', to: 'documents#index'
+  #Route for the ajax put request to update the current_user's editor's document's text
   put '/documents', to: 'documents#update'
 
->>>>>>> updated controllers, models, views, and javascript to get editors working
+  #Route for the about us page 
   get '/about', to: 'sites#about'
   #Route for the contact us page
   get '/contact', to: 'sites#contact'
