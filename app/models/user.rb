afe87a43234185843fe5980f22cd2b8d08c75f10
blairprophett <blaircuny@gmail.com>
2014-05-28 12:22:18 -0700

    starting with paperclip, added show page for users, now edit broken

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,14 @@ class User < ActiveRecord::Base
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 
   has_many :documents
+
+  # This method associates the attribute ":avatar" with a file attachment
+  has_attached_file :avatar, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
