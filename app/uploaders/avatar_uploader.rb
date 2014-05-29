d9bc8cc09bf15a0250eda6a0543dc4cc92b03f5e
blairprophett <blaircuny@gmail.com>
2014-05-29 15:17:10 -0700

    s3 working with carrierwave

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,14 +1,16 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # encoding: utf-8
+  include CarrierWave::MimeTypes
+  process :set_content_type
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # includesCarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  #storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
