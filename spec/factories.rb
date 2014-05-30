b41c56de180093fbec070a513a8a69bbb2a264d3
stevenmiller888 <stevenmiller888@me.com>
2014-05-29 19:13:18 -0700

    added sam's tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,24 @@
 FactoryGirl.define do
 
-  factory :user do
+  sequence(:profile_name) { |n| "KrispyKremes#{n}" }
+  sequence(:email) { |n| "KrispyKremes#{n}@http://email.com" } 
+
+  factory :user, aliases: [:friend] do
     full_name "John Doe"
-    profile_name "JohnDoe123"
+    profile_name 
     password_confirmation "password"
-    email "email@email.com"
+    email 
     password "password"
   end
+  
+  factory :document do
+    text "hello world"
+    user 
+    friendship_id 1
+  end
 
+  factory :friendship do
+    user_id 1
+    friend_id 2
+  end
 end
